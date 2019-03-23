eb10c652246d3ff34526d259971ff10e13aba967
Alex Sato <as4628@columbia.edu>
2019-03-23 17:59:05 -0400

    fixing callback url

--- pass.js
+++ pass.js
@@ -9,7 +9,7 @@ const app = require("./server");
 passport.use(new BnetStrategy({
     clientID: BNET_ID,
     clientSecret: BNET_SECRET,
-    callbackURL: "/auth/bnet/callback", //us.api.blizzard.com/wow/character/Crushridge/Akron",
+    callbackURL: "https://vast-journey-16073.herokuapp.com/auth/bnet/callback", //us.api.blizzard.com/wow/character/Crushridge/Akron",
     region: "us"
 },
     function (accessToken, refreshToken, profile, cb) {
